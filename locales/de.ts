adfdb9cbbdcc8f0c8217074fdbaabb7915c766ea
truFelix <trutschnigfelix@gmail.com>
2026-02-22 20:56:19 +0100

    text update
    Signed-off-by: truFelix <trutschnigfelix@gmail.com>

--- locales/de.ts
+++ locales/de.ts
@@ -1,5 +1,8 @@
 export default {
-  "23:25 description": "Eine Sammlung an Songs von 2023 bis 2025",
+  "23:25 description": `Eine Sammlung an Songs von 2023 bis 2025
+Experimente, die ich in ganze Songs ausgebaut habe.
+Verwendung von verschiedensten Instrumenten und Genres.
+Ich habe viel gelernt während dem Arrangen, Mixen, Schreiben und Soundauswahl sowie Verwendung von Effekten.`,
   "portfolio": "Portfolio",
   "music": "Musik",
   "music.description": `Projekte an denen ich als Produzent, Komponist oder Sound-Designer arbeite.`,
--- locales/en.ts
+++ locales/en.ts
@@ -1,7 +1,7 @@
 export default {
 	"23:25 description": `A collection of songs from 2023 till 2025.
 Songs, that started as experiments or snippets, that I built out to full songs.
-All genres and instruments.
+Contains all genres and instruments.
 I learned a lot during arranging, mixing, writing, sound selection, sound design and the use of effects.`,
 	"portfolio": "Portfolio",
 	"music": "Music",
